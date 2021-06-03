522afaf70f3659fa9e311222b6a354d479e945e7
Jean C. Contreras <jean.cntrs@gmail.com>
2021-06-02 23:52:24 -0400

    Added - Searches model

---
+++ models/searches.js
@@ -0,0 +1,15 @@
+class Searches {
+    history = ['Cabildo', 'San Felipe', 'Viña del Mar', 'Santiago'];
+
+    constructor() {
+
+    }
+
+    async searchCity(city = '') {
+        console.log(city);
+
+        return [];
+    }
+}
+
+module.exports = Searches;
